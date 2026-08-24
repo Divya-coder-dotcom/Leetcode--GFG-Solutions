a92def59e45280229c321ecb5f09918992813ff4
Divya-coder-dotcom <divyasinghal2009052@gmail.com>
2026-08-25 02:35:06 +0530

    Time: 0 ms (100%), Space: 15.5 MB (21.17%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void solve(string ip, string op, vector<string>& ans)
+    {
+        if(ip.length()==0)
+        {
+            ans.push_back(op);
+            return;
+        }
+        if(isalpha(ip[0]))
+        {
+            string op1=op;
+            string op2=op;
+            op1.push_back(toupper(ip[0]));
+            op2.push_back(tolower(ip[0]));
+            ip.erase(ip.begin()+0);
+            solve(ip,op1,ans);
+            solve(ip,op2,ans);
+        }
+
+        else
+        {
+            string op1= op;
+            op1.push_back(ip[0]);
+            ip.erase(ip.begin()+0);
+            solve(ip,op1,ans);
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        string ip=s;
+        string op="";
+        vector<string> ans;
+        solve(ip,op,ans);
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
